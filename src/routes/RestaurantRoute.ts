a076cae7f06a6a10724bfcfce741e9825bf51839
Ramsay <ramsayveitch123@gmail.com>
2024-03-01 20:55:43 +0000

    stripe has been setup some minor bugs need fixing (400) on one request

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -2,13 +2,26 @@ import express from "express";
 import { param } from "express-validator";
 import RestaurantController from "../controllers/RestaurantController";
 
+const router = express.Router();
 
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId parameter must be valid"),
+    RestaurantController.getRestaurant
+);
 
+router.get(
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City parameter must be valid"),
+  RestaurantController.searchRestaurant
+);
 
-const router = express.Router()
-
-
-router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("City parameter must be valid"),
-RestaurantController.searchRestaurant)
-
-export default router
+export default router;
